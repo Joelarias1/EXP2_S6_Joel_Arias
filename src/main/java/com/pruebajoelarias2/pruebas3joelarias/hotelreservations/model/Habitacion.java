bf0a2fd0eebf165d77abcb5a1e8c4251f087bbf9
Joel Arias <joelariasparra3@gmail.com>
2024-09-10 19:02:43 -0300

    Creacion de tables y claves foraneas

--- src/main/java/com/pruebajoelarias2/pruebas3joelarias/hotelreservations/model/Habitacion.java
+++ src/main/java/com/pruebajoelarias2/pruebas3joelarias/hotelreservations/model/Habitacion.java
@@ -1,39 +1,42 @@
 package com.pruebajoelarias2.pruebas3joelarias.hotelreservations.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Entity
+@Table(name = "HABITACION")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Habitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "HABITACION_ID")
     private Long id;
+
+    @Column(name = "TIPO", nullable = false)
     private String tipo;
+
+    @Column(name = "PRECIO_POR_NOCHE", nullable = false)
     private double precioPorNoche;
+
+    @Column(name = "DISPONIBLE", nullable = false)
     private boolean disponible;
+
+    // Relación muchos a uno con Hotel (FK)
+    @ManyToOne
+    @JoinColumn(name = "HOTEL_ID", nullable = false) 
     private Hotel hotel;
 
-    // Constructor
-    public Habitacion(Long id, String tipo, double precioPorNoche, boolean disponible, Hotel hotel) {
-        this.id = id;
-        this.tipo = tipo;
-        this.precioPorNoche = precioPorNoche;
-        this.disponible = disponible;
-        this.hotel = hotel;
-    }
-
-    // Getters
-    public Long getId() {
-        return id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public double getPrecioPorNoche() {
-        return precioPorNoche;
-    }
-
-    public boolean isDisponible() {
-        return disponible;
-    }
-
-    public Hotel getHotel() {
-        return hotel;
-    }
 }
--- src/main/java/com/pruebajoelarias2/pruebas3joelarias/hotelreservations/model/Reserva.java
+++ src/main/java/com/pruebajoelarias2/pruebas3joelarias/hotelreservations/model/Reserva.java
@@ -1,31 +1,37 @@
 package com.pruebajoelarias2.pruebas3joelarias.hotelreservations.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "RESERVA")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RESERVA_ID")
     private Long id;
+
+    @Column(name = "NOMBRE_CLIENTE", nullable = false)
     private String nombreCliente;
+
+    // (FK)
+    @ManyToOne
+    @JoinColumn(name = "HABITACION_ID", nullable = false)
     private Habitacion habitacion;
-
-
-    // Constructor
-    public Reserva(Long id, String nombreCliente, Habitacion habitacion) {
-        this.id = id;
-        this.nombreCliente = nombreCliente;
-        this.habitacion = habitacion;
-
-    }
-
-    // Getters
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombreCliente() {
-        return nombreCliente;
-    }
-
-    public Habitacion getHabitacion() {
-        return habitacion;
-    }
-
 
 }
